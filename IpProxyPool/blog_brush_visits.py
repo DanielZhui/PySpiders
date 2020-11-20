202ccf5efdbc90da2e995b59953f14e9298c7bf7
Wollens <zh2017728@163.com>
2020-11-20 17:08:44 +0800

    release 0.0.1

--- IpProxyPool/blog_brush_visits.py
+++ IpProxyPool/blog_brush_visits.py
@@ -1,3 +1,4 @@
+import os
 import json
 import requests
 from lxml import etree
@@ -12,23 +13,20 @@ def get_blog_list(blog_list_url):
     data = selector.xpath('//*[@id="articleMeList-blog"]/div[2]/div//a/@href')
     return data
 
-def get_proxyer():
-    f = open('./useful.txt', 'rb')
-    opener_list = []
+def brush_visits(data):
+    f = open(os.path.join(os.path.dirname(__file__), 'useful.txt'), 'rb')
     for line in f.readlines():
-        line = json.dumps(line)
-        print(line, type(line))
+        print(line)
+        line = json.loads(line.decode('utf8'))
         proxy_ip = {line.get('type'): line.get('host')}
         proxy_support = request.ProxyHandler(proxy_ip)
         opener = request.build_opener(proxy_support)
         opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36')]
-        opener_list.append(request.install_opener(opener))
-    return opener_list
+        for link in data:
+            print('brush>>>', link)
+            request.urlopen(link)
 
-def brush_visits(data, opener_list):
-        pass
 
 blog_list_url = 'https://blog.csdn.net/DanielJackZ?spm=1011.2124.3001.5113'
-get_blog_list(blog_list_url)
-
-get_proxyer()
+data = get_blog_list(blog_list_url)
+brush_visits(data)
