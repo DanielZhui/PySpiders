c8fe811f55755db5376a52d20ed599cf32a58ce0
Wollens <zh2017728@163.com>
2021-01-07 22:08:53 +0800

    flask conect redis

--- flaskProxyPool/web.py
+++ flaskProxyPool/web.py
@@ -1,11 +1,18 @@
+import redis
 from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+
+rdb = redis.StrictRedis(host='127.0.0.1', port=6379, db=1)
 
 app = Flask(__name__)
 
 
 @app.route('/api/proxyip', methods=['GET'])
 def get_proxy_ip():
+    rdb.set('key', 'value')
+    print(rdb.get('key').decode())
     return 'proxy ip'
 
 
-app.run()
+if __name__ == "__main__":
+    app.run()
